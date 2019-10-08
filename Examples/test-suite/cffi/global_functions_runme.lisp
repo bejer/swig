818bbdf6e626ab03a64b653e51bf2d25f06a57f3
bejer <bejerdk@gmail.com>
2019-10-08 21:55:24 +0200

    Make variable getter and setter wrappers appear at the end of the wrapped source file. Avoiding referencing undeclared variables. The test case simple_array.i caused this to be an issue.

--- Examples/test-suite/cffi/global_functions_runme.lisp
+++ Examples/test-suite/cffi/global_functions_runme.lisp
@@ -4,8 +4,8 @@
   (unless (= a b)
     (error (format nil "Failed: ~A != ~A" a b))))
 
-(global_functions:global_void)
+(global_functions:global-void)
 
-(check (global_functions:global_one 1) 1)
+(check (global_functions:global-one 1) 1)
 
-(check (global_functions:global_two 2 2) 4)
+(check (global_functions:global-two 2 2) 4)
